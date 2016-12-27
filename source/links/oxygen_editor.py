6c425448efcec3bc546eb5427f82e9d8d5d76f69
Mark Hoeber <mhoeber@cimpress.com>
2016-12-27 11:11:25 -0500

    dita

---
+++ source/links/oxygen_editor.py
@@ -0,0 +1,8 @@
+
+import link
+
+link_name = "oXygen XML Editor"
+user_text = "oXygen XML Editor"
+url = "https://www.oxygenxml.com/xml_author.html"
+
+link.xref_links.update({link_name: (user_text, url)})
