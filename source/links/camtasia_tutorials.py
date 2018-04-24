6811b2b4151810b7f0b894aaf0efe118275e6975
MarkHoeber <mark.hoeber@cybereason.com>
2018-04-23 21:37:05 -0400

    add camtasia info

---
+++ source/links/camtasia_tutorials.py
@@ -0,0 +1,8 @@
+
+import link
+
+link_name = "Camtasia Tutorials"
+user_text = "Camtasia Tutorials"
+url = "https://www.techsmith.com/tutorial-camtasia.html"
+
+link.xref_links.update({link_name: (user_text, url)})
